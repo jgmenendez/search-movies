2b4ed20f08e873725b1839b436a4a2f566c50b49
Javier Garcia Menendez <javiergme@servicioexterno.inditex.com>
2020-05-26 12:17:01 +0200

    Buscador de películas con rutas SPA

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
 import React, { Component } from 'react';
-import { Title } from './components/title';
-import { SearchForm } from './components/searchForm';
-import { MoviesList } from './components/moviesList';
+import { Switch, Route } from 'react-router-dom';
+import { Detail } from './pages/detail';
+import { Home } from './pages/home';
+import { NotFound } from './pages/notFound';
 
 import './App.css';
 import 'bulma/css/bulma.css';
 
 class App extends Component {
-  state = { usedSearch: false, results: [] }
-
-  _handleResults = (results) => {
-    this.setState({ usedSearch: true, results });
-  }
-
-  _renderResults = () => {
-    return this.state.results.length === 0 
-      ? <p>Sorry! Results not found!</p> 
-      : <MoviesList movies={ this.state.results } /> 
-  }
-
   render() {
     return (
       <div className="App">
-        <Title>Search Movies</Title>
-        <div className="SearchForm-wrapper">
-          <SearchForm onResults={ this._handleResults } />
-        </div>
-        {
-          this.state.usedSearch
-            ? this._renderResults()
-            : <small>Use the form to search a movie</small>
-        }
+        <Switch>
+          <Route exact path='/' component={ Home } />
+          <Route path='/detail/:movieId' component={ Detail } />
+          <Route component={ NotFound } />
+        </Switch>
       </div>
     );
   }
